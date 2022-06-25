01a47ea5faad299e6f6bfb4f1c6a740d6a06f295
Felipe Almeida Correa <felipealmeidacorrea@outlook.com.br>
2022-06-24 20:03:46 -0400

    adjustments

--- js/app.js
+++ js/app.js
@@ -45,9 +45,9 @@ $(document).ready(function() {
     });
 
     $(".shelf__list").slick({
-        lazyLoad: "ondemand",
         waitForAnimate: false,
-        autoplay: false,
+        autoplay: !0,
+        autoplaySpeed: 4500,
         infinite: true,
         slidesToShow: 5,
         slidesToScroll: 1,
@@ -63,26 +63,7 @@ $(document).ready(function() {
                 <path d="M18.9047 17.2436L4.6633 31.4849C3.97646 32.1717 2.86291 32.1717 2.17613 31.4849L0.515117 29.8238C-0.170556 29.1382 -0.171875 28.0269 0.512186 27.3396L11.7987 16L0.512186 4.66039C-0.171875 3.9731 -0.170556 2.86182 0.515117 2.17615L2.17613 0.515134C2.86298 -0.171711 3.97653 -0.171711 4.6633 0.515134L18.9046 14.7564C19.5914 15.4432 19.5914 16.5567 18.9047 17.2436Z" fill="#353535"/>
             </svg>
         </button>`,
-        dots: false,
-        // responsive: [{
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 1,
-        //             centerMode: true,
-        //             centerPadding: '30px'
-        //         }
-        //     }
-        // {
-        //     breakpoint: 1300,
-        //     settings: {
-        //         slidesToShow: 2,
-        //         slidesToScroll: 1,
-        //         centerMode: true,
-        //         centerPadding: '30px'
-        //     }
-        // }
-        // ]
+        dots: false
     });
 
 });
